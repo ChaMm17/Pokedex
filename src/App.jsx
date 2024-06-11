ed94086aae8ffda466ebfd6bfbbdae54ec8baa61
ChachaMm17 <charlotte.morvant@gmail.com>
2024-06-11 17:54:09 +0200

    création d'un props

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,20 @@ function App() {
 
   return (
     <div>
-      <PokemonCard />
+      <PokemonCard pokemon={pokemonList[0]} />
     </div>
   )
 }
 
 export default App
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,23 +1,9 @@
-function PokemonCard() {
-
-    const pokemon = pokemonList[1];
+function PokemonCard({ pokemon }) {
     return <figure>{pokemon.imgSrc != null ? <img src={pokemon.imgSrc} alt={pokemon.name}></img> : <p>???</p>}
         <figcaption>{pokemon.name}</figcaption>
     </figure >
 
 }
-
-
-const pokemonList = [
-    {
-        name: "bulbasaur",
-        imgSrc:
-            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-        name: "mew",
-    },
-];
 
 export default PokemonCard;
 
