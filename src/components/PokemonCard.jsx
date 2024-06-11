57780fa1c24af34b0ee262551ccbff9b5f6e8f59
ChachaMm17 <charlotte.morvant@gmail.com>
2024-06-11 16:18:56 +0200

    Ajout d'un composant dynamique

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,8 +1,23 @@
 function PokemonCard() {
-    return <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur"></img>
-        <figcaption>Bulbasaur</figcaption>
+
+    const pokemon = pokemonList[1];
+    return <figure>{pokemon.imgSrc != null ? <img src={pokemon.imgSrc} alt={pokemon.name}></img> : <p>???</p>}
+        <figcaption>{pokemon.name}</figcaption>
     </figure >
+
 }
 
+
+const pokemonList = [
+    {
+        name: "bulbasaur",
+        imgSrc:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "mew",
+    },
+];
+
 export default PokemonCard;
+
