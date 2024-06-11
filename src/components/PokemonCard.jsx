367bb948147e138dc9da48b6f994b4d44c3142d6
ChachaMm17 <charlotte.morvant@gmail.com>
2024-06-11 18:32:01 +0200

    Ajout de PropTypes pour définir le type des props

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,9 +1,19 @@
+import PropTypes from "prop-types";
+
 function PokemonCard({ pokemon }) {
     return <figure>{pokemon.imgSrc != null ? <img src={pokemon.imgSrc} alt={pokemon.name}></img> : <p>???</p>}
         <figcaption>{pokemon.name}</figcaption>
     </figure >
 
 }
+
+PokemonCard.PropTypes = {
+    pokemon: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        imgSrc: PropTypes.string,
+    })
+}
+
 
 export default PokemonCard;
 
